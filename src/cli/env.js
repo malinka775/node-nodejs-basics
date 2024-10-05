71444712839533dc0d2ee022c1ba84e79581072f
Alina <malinka775@yandex.ru>
2024-10-05 19:53:40 +0300

    feat: implement parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const TARGET_PREFIX = 'RSS_';
+    const envObject = process.env;
+    const targetKeys = Object.keys(envObject).filter(item => item.startsWith(TARGET_PREFIX));
+    const result = targetKeys.map(key => `${key}=${envObject[key]}`).join('; ');
+    console.log(result);
 };
 
 parseEnv();

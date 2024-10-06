3a05b5636987ab6cbffc513b22978ba3a3696c34
Alina <malinka775@yandex.ru>
2024-10-06 19:19:14 +0300

    feat: implement decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,32 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+import { isDestinationExisting } from '../helpers.js';
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const execFilePath = fileURLToPath(import.meta.url);
+        const execDirPath = dirname(execFilePath);
+        const sourcePath = join(execDirPath, 'files', 'archive.gz');
+        const destinationPath = join(execDirPath, 'files', 'fileToCompress.txt');
+
+        if (!await isDestinationExisting(sourcePath)) {
+            console.log('Operation failed: unfortunately the source file is not found');
+            return
+        }
+
+        const unzip = createGunzip();
+        const promisifiedPipeline = promisify(pipeline);
+        const rs = createReadStream(sourcePath);
+        const ws = createWriteStream(destinationPath);
+
+        promisifiedPipeline(rs, unzip, ws);
+    } catch(e) {
+        throw e
+    }
 };
 
 await decompress();

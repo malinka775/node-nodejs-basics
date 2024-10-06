a6d37c2dedc066499a5a99e22dc0f4e285901037
Alina <malinka775@yandex.ru>
2024-10-06 17:02:47 +0300

    feat: implement write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,30 @@
+import { createWriteStream } from 'fs';
+import { stdin } from 'process';
+import { pipeline } from 'stream';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const write = async () => {
-    // Write your code here 
+    try {
+        const execFilePath = fileURLToPath(import.meta.url);
+        const execDirPath = dirname(execFilePath);
+        const targetFilePath = join(execDirPath, 'files', 'fileToWrite.txt');
+
+        const myWriteStream = createWriteStream(targetFilePath);
+        const myReadStream = stdin;
+
+        pipeline(
+            myReadStream,
+            myWriteStream,
+            (e) => {
+                if(e){
+                    throw e
+                }
+            }
+        );
+    } catch(err) {
+        throw err;
+    }
 };
 
 await write();

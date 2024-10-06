8648fc99de4d9d43ca47fb5d3d897d504a29bc22
Alina <malinka775@yandex.ru>
2024-10-06 18:59:50 +0300

    feat: implement compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,36 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+import { isDestinationExisting } from '../helpers.js';
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        const execFilePath = fileURLToPath(import.meta.url);
+        const execDirPath = dirname(execFilePath);
+        const sourcePath = join(execDirPath, 'files', 'fileToCompress.txt');
+        const destinationPath = join(execDirPath, 'files', 'archive.gz');
+        
+        if (!await isDestinationExisting(sourcePath)) {
+            console.log('Operation failed: unfortunately the source file is not found');
+            return
+        }
+        
+        const gzip = createGzip();
+        const rs = createReadStream(sourcePath);
+        const ws = createWriteStream(destinationPath);
+        
+        const promisifiedPipeline = promisify(pipeline);
+        await promisifiedPipeline(
+            rs,
+            gzip,
+            ws
+        );
+    } catch(e) {
+        throw e;
+    }
 };
 
 await compress();
